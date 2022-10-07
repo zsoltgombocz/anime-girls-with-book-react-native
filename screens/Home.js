6382b774a5b61451860f614b7325b21d33a4a073
zsoltgombocz <zsolt.gombocz00@gmail.com>
2022-10-07 15:00:18 +0200

    added data splitting into half for masonry + masonry comp

--- screens/Home.js
+++ screens/Home.js
@@ -5,18 +5,28 @@ import { DataContext } from "../states/DataContext";
 import AppLoader from "../components/AppLoader";
 import Navigation from "../components/Navigation";
 import Header from "../components/Header";
+import Masonry from "../components/masonry/Masonry";
+import MasonryImage from "../components/masonry/MasonryImage";
 
-const Home = () => {
+const Home = ({ fontsloaded }) => {
 	const { data } = useContext(DataContext);
 	const fadeIn = useRef(new Animated.Value(0)).current;
 	const [expand, setExpand] = useState(true);
 	const [scroll, setScroll] = useState(0);
 	const [scrollDirection, setScrollDirection] = useState("");
+	const [masonryData, setMasonryData] = useState([null, null]);
 
 	useEffect(() => {
-		console.log(data);
-		if (data.data.length > 0) {
-			setExpand(false);
+		if (data && data.data && fontsloaded) {
+			if (data.data.length > 0) {
+				console.log(data.data);
+				let masonryImages = data.data.map((img) => <MasonryImage imgObj={img} />);
+				setMasonryData([
+					masonryImages.slice(0, masonryImages.length / 2),
+					masonryImages.slice(masonryImages.length / 2),
+				]);
+				setExpand(false);
+			}
 		}
 	}, [data]);
 
@@ -40,6 +50,7 @@ const Home = () => {
 			<View className="bg-navbar top-0 h-10 m-0 p-0 absolute w-screen"></View>
 			<SafeAreaView className="h-screen text-center flex m-0 p-0">
 				<Header show={!expand} text={"All Girls With Book"} />
+				<Masonry rowA={masonryData[0]} rowB={masonryData[1]} />
 			</SafeAreaView>
 			<Navigation expanded={expand} scroll={scrollDirection} />
 		</View>
