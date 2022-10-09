1e6f8897c953c5c136ec7ba493d1b6037346a694
zsoltgombocz <zsolt.gombocz00@gmail.com>
2022-10-09 13:25:00 +0200

    added next page fetch

--- states/DataContext.js
+++ states/DataContext.js
@@ -6,21 +6,34 @@ export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
 	const [data, setData] = useState([]);
+	const [page, setPage] = useState({});
+	const [requestInProgress, setRequestInProgress] = useState(false);
 
 	useEffect(() => {
-		const fetchPage = async (page = 1, filter = "all") => {
-			try {
-				const { data } = await api(API_URL).get(`?page=${page}&filter=${filter}`);
-				setData(data);
-			} catch (error) {
-				return new Error(error);
-			}
-		};
-
 		setTimeout(() => {
 			fetchPage();
 		}, 1000);
 	}, []);
 
-	return <DataContext.Provider value={{ data }}>{children}</DataContext.Provider>;
+	const fetchPage = async (page = 1, filter = "all") => {
+		try {
+			const { data } = await api(API_URL).get(`?page=${page}&filter=${filter}`);
+			const { total, total_page, current_page, from, to } = data;
+			setPage({ total, total_page, current_page, from, to, filter });
+			setData((prev) => prev.concat(data.data));
+		} catch (error) {
+			return new Error(error);
+		}
+	};
+
+	const getNextPage = async () => {
+		if (requestInProgress) return;
+		if (page.current_page + 1 > page.total_page) return;
+
+		setRequestInProgress(true);
+		await fetchPage(page.current_page + 1, page.filter);
+		setRequestInProgress(false);
+	};
+
+	return <DataContext.Provider value={{ data, getNextPage }}>{children}</DataContext.Provider>;
 };
