d6abe76bbccf563fe1ab63e0ec8b7ba8fd606430
zsoltgombocz <zsolt.gombocz00@gmail.com>
2022-10-09 16:01:00 +0200

    touchable imagesow open viewer

--- components/masonry/MasonryImage.js
+++ components/masonry/MasonryImage.js
@@ -1,31 +1,50 @@
-import React, { useState, useEffect } from "react";
-import { Dimensions, Image, View, Text, ActivityIndicator } from "react-native";
+import React, { useState, useContext } from "react";
+import { Dimensions, Image, View, Text, ActivityIndicator, TouchableHighlight } from "react-native";
+import { ImageDisplayContext } from "../../states/ImageDisplayContext";
 import Capsule from "../Capsule";
+import DisplayImage from "./DisplayImage";
 
-const MasonryImage = ({ imgObj }) => {
+const MasonryImage = ({ imgObj, scrollEnableFc }) => {
 	const [loaded, setLoaded] = useState(false);
 	const [imageRatio, setImageRation] = useState(1);
+	const { data, setData } = useContext(ImageDisplayContext);
 
 	return (
-		<View className="mb-4">
-			{!loaded && (
-				<View className="absolute w-full h-full items-center justify-center">
-					<ActivityIndicator color="black" />
-				</View>
-			)}
-			<Image
-				onLoad={({
-					nativeEvent: {
-						source: { width, height },
-					},
-				}) => {
-					setImageRation(width / height);
-					setLoaded(true);
+		<View className="mb-4 z-1">
+			<TouchableHighlight
+				className="z-1 relative"
+				onPress={() => {
+					if (!data.showing) {
+						setData({
+							show: true,
+							url: imgObj.url,
+							ratio: imageRatio,
+						});
+					}
 				}}
-				style={{ aspectRatio: imageRatio }}
-				className={"rounded-lg"}
-				source={{ uri: imgObj.url }}
-			/>
+				underlayColor="white"
+			>
+				<>
+					{!loaded && (
+						<View className="absolute w-full h-full items-center justify-center">
+							<ActivityIndicator color="black" />
+						</View>
+					)}
+					<Image
+						onLoad={({
+							nativeEvent: {
+								source: { width, height },
+							},
+						}) => {
+							setImageRation(width / height);
+							setLoaded(true);
+						}}
+						style={{ aspectRatio: imageRatio }}
+						className={"rounded-lg z-1"}
+						source={{ uri: imgObj.url }}
+					/>
+				</>
+			</TouchableHighlight>
 			{loaded && <Capsule text={imgObj.category} extraClass={"mt-2"} />}
 		</View>
 	);
