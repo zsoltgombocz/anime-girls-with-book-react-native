f8eb4cece41a79d5b8e8fb5e5429adf2c0dd956e
zsoltgombocz <zsolt.gombocz00@gmail.com>
2022-10-11 10:25:55 +0200

    removed image display comp + unused imports

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,13 @@
-import { View, Text, Image, Animated, ScrollView } from "react-native";
+import { View } from "react-native";
 import React, { useContext, useRef, useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { DataContext } from "../states/DataContext";
 import Navigation from "../components/Navigation";
 import Header from "../components/Header";
 import Masonry from "../components/masonry/Masonry";
-import ImageDisplayRoot from "../components/ImageDisplayRoot";
 
 const Home = ({ fontsloaded }) => {
 	const { data } = useContext(DataContext);
-	const fadeIn = useRef(new Animated.Value(0)).current;
 	const [expand, setExpand] = useState(true);
 	const [scroll, setScroll] = useState(0);
 	const [scrollDirection, setScrollDirection] = useState("");
@@ -43,7 +41,6 @@ const Home = ({ fontsloaded }) => {
 		<View className="h-full w-screen bg-gray">
 			<View className="bg-navbar top-0 h-10 m-0 p-0 absolute w-screen z-20"></View>
 			<SafeAreaView className="h-screen text-center flex m-0 p-0 relative">
-				<ImageDisplayRoot />
 				<Header show={!expand} text={"All Girls With Book"} />
 				<Masonry data={masonryData} />
 			</SafeAreaView>
