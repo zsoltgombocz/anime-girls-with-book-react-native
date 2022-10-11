9eef2af554c6bbdcf915c4506ffa3d43a4416137
zsoltgombocz <zsolt.gombocz00@gmail.com>
2022-10-11 17:02:51 +0200

    added header title based on current screen

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -5,13 +5,14 @@ import { DataContext } from "../states/DataContext";
 import Navigation from "../components/Navigation";
 import Header from "../components/Header";
 import Masonry from "../components/masonry/Masonry";
+import { NavigationContext } from "../states/NavigationContext";
 
 const MainLayout = ({ children, fontsloaded }) => {
 	const { data } = useContext(DataContext);
 	const [expand, setExpand] = useState(true);
 	const [scroll, setScroll] = useState(0);
 	const [scrollDirection, setScrollDirection] = useState("");
-	const [masonryData, setMasonryData] = useState(null);
+	const { getCurrentScreen } = useContext(NavigationContext);
 
 	useEffect(() => {
 		if (data && data && fontsloaded) {
@@ -39,7 +40,12 @@ const MainLayout = ({ children, fontsloaded }) => {
 		<View className="h-full w-screen bg-gray">
 			<View className="bg-navbar top-0 h-10 m-0 p-0 absolute w-screen z-20"></View>
 			<SafeAreaView className="h-screen text-center flex m-0 p-0 relative">
-				<Header show={!expand} text={"All Girls With Book"} />
+				<Header
+					show={!expand}
+					text={
+						getCurrentScreen() === "Home" ? "All Girls With Book" : getCurrentScreen()
+					}
+				/>
 				{children}
 			</SafeAreaView>
 			<Navigation expanded={expand} scroll={scrollDirection} />
