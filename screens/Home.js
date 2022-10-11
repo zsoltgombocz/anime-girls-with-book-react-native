3fb52aea6233a32dc8994adc94347f2e9a5915f3
zsoltgombocz <zsolt.gombocz00@gmail.com>
2022-10-11 11:59:16 +0200

    refactor

--- screens/Home.js
+++ screens/Home.js
@@ -6,47 +6,18 @@ import Navigation from "../components/Navigation";
 import Header from "../components/Header";
 import Masonry from "../components/masonry/Masonry";
 
-const Home = ({ fontsloaded }) => {
+const Home = () => {
 	const { data } = useContext(DataContext);
-	const [expand, setExpand] = useState(true);
-	const [scroll, setScroll] = useState(0);
-	const [scrollDirection, setScrollDirection] = useState("");
 	const [masonryData, setMasonryData] = useState(null);
-
 	useEffect(() => {
-		if (data && data && fontsloaded) {
+		if (data) {
 			if (data.length > 0) {
 				setMasonryData(data);
-				setExpand(false);
 			}
 		}
 	}, [data]);
 
-	const handleScroll = (e) => {
-		let currentOffset = e.nativeEvent.contentOffset.y;
-		const dif = currentOffset - (scroll || 0);
-
-		if (Math.abs(dif) < 3) {
-			setScrollDirection("unclear");
-		} else if (dif <= 0) {
-			setScrollDirection("up");
-		} else {
-			setScrollDirection("down");
-		}
-
-		setScroll(currentOffset);
-	};
-
-	return (
-		<View className="h-full w-screen bg-gray">
-			<View className="bg-navbar top-0 h-10 m-0 p-0 absolute w-screen z-20"></View>
-			<SafeAreaView className="h-screen text-center flex m-0 p-0 relative">
-				<Header show={!expand} text={"All Girls With Book"} />
-				<Masonry data={masonryData} />
-			</SafeAreaView>
-			<Navigation expanded={expand} scroll={scrollDirection} />
-		</View>
-	);
+	return <Masonry data={masonryData} />;
 };
 
 export default Home;
