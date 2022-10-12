812ea19a1ecce653bca0850b422afa3e71e27243
zsoltgombocz <zsolt.gombocz00@gmail.com>
2022-10-12 16:48:23 +0200

    stores current screen and title + some func

--- states/NavigationContext.js
+++ states/NavigationContext.js
@@ -1,20 +1,35 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 export const NavigationContext = createContext();
 
 export const NavigationProvider = ({ children }) => {
-	const [navigationData, setNavigationData] = useState({
-		screen: "Home",
-	});
-
-	const setCurrentScreen = (screenName) => {
-		setNavigationData({ ...navigationData, screen: screenName });
+	const ScreenTitleMap = {
+		Home: "All Girls With Book",
+		Filter: "Filtered Girls",
 	};
 
-	const getCurrentScreen = () => navigationData.screen;
+	const [navigationData, setNavigationData] = useState({
+		screen: "Home",
+		screenTitle: ScreenTitleMap["Home"] || "",
+	});
+
+	const setCurrentScreen = (screenName, customTitle, forceEmpty = false) => {
+		let screenTitle = title(screenName, customTitle, forceEmpty);
+		setNavigationData({ ...navigationData, screen: screenName, screenTitle });
+	};
+
+	const getCurrentScreen = () => navigationData;
+
+	const title = (screenName, title, forceEmpty = false) =>
+		!forceEmpty ? (title ? title : ScreenTitleMap[screenName] || screenName) : "";
+
+	const setTitle = (customText) => {
+		let screenTitle = title(navigationData.screen, customText);
+		setNavigationData({ ...navigationData, screenTitle });
+	};
 
 	return (
-		<NavigationContext.Provider value={{ setCurrentScreen, getCurrentScreen }}>
+		<NavigationContext.Provider value={{ setCurrentScreen, getCurrentScreen, setTitle }}>
 			{children}
 		</NavigationContext.Provider>
 	);
