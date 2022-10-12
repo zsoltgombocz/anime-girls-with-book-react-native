a7725006777e5838885d6374c854e3655424f1b3
zsoltgombocz <zsolt.gombocz00@gmail.com>
2022-10-12 16:48:43 +0200

    global state to store images, and functions to manipulate

--- states/DataContext.js
+++ states/DataContext.js
@@ -6,10 +6,12 @@ export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
 	const [data, setData] = useState([]);
-	const [page, setPage] = useState({});
+	const [categories, setCategories] = useState([]);
+	const [page, setPage] = useState([]);
 	const [requestInProgress, setRequestInProgress] = useState(false);
 
 	useEffect(() => {
+		getCategories();
 		setTimeout(() => {
 			fetchPage();
 		}, 1000);
@@ -20,6 +22,7 @@ export const DataProvider = ({ children }) => {
 			const { data } = await api(API_URL).get(`?page=${page}&filter=${filter}`);
 			const { total, total_page, current_page, from, to } = data;
 			setPage({ total, total_page, current_page, from, to, filter });
+
 			setData((prev) => prev.concat(data.data));
 		} catch (error) {
 			return new Error(error);
@@ -35,5 +38,24 @@ export const DataProvider = ({ children }) => {
 		setRequestInProgress(false);
 	};
 
-	return <DataContext.Provider value={{ data, getNextPage }}>{children}</DataContext.Provider>;
+	const getCategories = async () => {
+		try {
+			const { data } = await api(API_URL).get(`categories`);
+			let categs = data;
+			categs.unshift("all");
+			setCategories(categs);
+		} catch (error) {
+			return new Error(error);
+		}
+	};
+
+	const clearData = () => {
+		setData([]);
+	};
+
+	return (
+		<DataContext.Provider value={{ data, getNextPage, fetchPage, categories, page, clearData }}>
+			{children}
+		</DataContext.Provider>
+	);
 };
