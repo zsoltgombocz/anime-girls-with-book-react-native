3619792843be9b586a197685287919e2d8a1833e
zsoltgombocz <zsolt.gombocz00@gmail.com>
2022-10-12 16:28:28 +0200

    smoll fixes in style

--- components/Capsule.js
+++ components/Capsule.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-const Capsule = ({ text, extraClass }) => {
+const Capsule = ({ text, extraClass, textClass }) => {
 	return (
 		<View
-			style={{ alignSelf: "flex-start", fontFamily: "Poppins_300Light" }}
+			style={{ alignSelf: "flex-start", fontFamily: "Poppins" }}
 			className={"rounded-full border border-navbar px-1 " + extraClass}
 		>
-			<Text classname="text-xs">{text}</Text>
+			<Text style={{ fontFamily: "Poppins" }} className={"text-xs " + textClass}>
+				{text}
+			</Text>
 		</View>
 	);
 };
