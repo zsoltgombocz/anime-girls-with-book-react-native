adb97d9cd88deef1318bf1ec6a69b6d0111069da
zsoltgombocz <zsolt.gombocz00@gmail.com>
2022-11-12 22:01:52 +0100

    simplified navigation context and added back btn handle

--- states/NavigationContext.js
+++ states/NavigationContext.js
@@ -1,35 +1,33 @@
 import { useState, createContext, useEffect } from "react";
+import { BackHandler, Alert } from "react-native";
 
 export const NavigationContext = createContext();
 
 export const NavigationProvider = ({ children }) => {
-	const ScreenTitleMap = {
-		Home: "All Girls With Book",
-		Filter: "Filtered Girls",
+	const handleBackPress = () => {
+		if (navigationData.screen !== "Home") {
+			setCurrentScreen("Home");
+		}
 	};
+
+	useEffect(() => {
+		BackHandler.addEventListener("hardwareBackPress", handleBackPress);
+
+		return () => BackHandler.removeEventListener("hardwareBackPress", handleBackPress);
+	}, []);
 
 	const [navigationData, setNavigationData] = useState({
 		screen: "Home",
-		screenTitle: ScreenTitleMap["Home"] || "",
 	});
 
-	const setCurrentScreen = (screenName, customTitle, forceEmpty = false) => {
-		let screenTitle = title(screenName, customTitle, forceEmpty);
-		setNavigationData({ ...navigationData, screen: screenName, screenTitle });
+	const setCurrentScreen = (screenName) => {
+		setNavigationData({ screen: screenName });
 	};
 
 	const getCurrentScreen = () => navigationData;
 
-	const title = (screenName, title, forceEmpty = false) =>
-		!forceEmpty ? (title ? title : ScreenTitleMap[screenName] || screenName) : "";
-
-	const setTitle = (customText) => {
-		let screenTitle = title(navigationData.screen, customText);
-		setNavigationData({ ...navigationData, screenTitle });
-	};
-
 	return (
-		<NavigationContext.Provider value={{ setCurrentScreen, getCurrentScreen, setTitle }}>
+		<NavigationContext.Provider value={{ setCurrentScreen, getCurrentScreen }}>
 			{children}
 		</NavigationContext.Provider>
 	);
